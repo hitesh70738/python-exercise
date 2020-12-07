f23758e828d5dd209e4dd426b44e12513f257296
hitesh70738 <hitesh70738@gmail.com>
2020-12-07 17:57:20 +0000

    updated challenge 1

--- Python - cotd/ex1.py
+++ Python - cotd/ex1.py
@@ -1,10 +1,13 @@
 def newstring(str):
 
     words = [word for word in str.split()]
+    # Create dict using 'words' item as keys. 
+    # This will remove any duplicates due.
+    # dictionary is then converted back into a list
 
     mylist = list(dict.fromkeys(words))
 
-    mynewlist = ' '.join(sorted(mylist))
+    mynewlist = ' '.join(sorted(mylist)) # returns a sorted list made out of the characters of the string
 
     return mynewlist
     
