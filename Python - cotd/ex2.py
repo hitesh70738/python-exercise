25877b8b7c397ad0bcd2d3810c569a7de94fb443
hitesh70738 <hitesh70738@gmail.com>
2020-12-08 16:59:53 +0000

    cotd2 complete

--- Python - cotd/ex2.py
+++ Python - cotd/ex2.py
@@ -3,15 +3,12 @@ def up_low(s):
     
     
     for char in s:
-        if char.isupper():
+        if char.isupper():     # isupper checks for every upper case letter
             a["upper"] += 1
-        elif char.islower():
+        elif char.islower():   # islower check for every lower case letter
             a["lower"] += 1
-        else:
-            pass
-
-    
-    print("Upper: ", a["upper"])
-    print("lower: ", a["lower"])
+                     
+    print("Upper -> ", a["upper"])
+    print("lower -> ", a["lower"])
 
 print(up_low("Hello world"))
